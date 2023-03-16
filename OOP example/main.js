5b6d1d225ed7858c525b6d7e6943b4fb81bacf5e
Âu Thiên Phước <126746510+thienphuoc261@users.noreply.github.com>
2023-03-16 21:54:39 +0700

    Add files via upload

---
+++ OOP example/main.js
@@ -0,0 +1,33 @@
+let MyDate = function(day, month, year){
+    this.day = day;
+    this.month = month;
+    this.year = year;
+
+    this.getDay = function(){
+        return this.day;
+    }
+    this.getMonth = function(){
+        return this.month;
+    }
+    this.getYear = function(){
+        return this.year;
+    }
+    this.setDay = function(day){
+        this.day = day;
+    }
+    this.setMonth = function(month){
+        this.month = month;
+    }
+    this.setYear = function(year){
+        this.year = year;
+    }
+}
+let date = new MyDate(2, 2, 2007);
+date.setDay(26);
+date.setMonth(01);
+date.setYear(2000);
+
+let day = date.getDay();
+let month = date.getMonth();
+let year = date.getYear();
+alert(day + "/" + month + "/" + year);
